efb2d1748e55cd856561a9e4fe2d453005f92ecd
muchadhesion <muchadhesion@none.com>
2024-10-16 18:04:54 +0100

    Coins and wands have metadata for collection

--- collector_area_2d.gd
+++ collector_area_2d.gd
@@ -1,6 +1,13 @@
 extends Area2D
 
+var weapon = null
+var coins = 0
+
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("collectable"):
 		area.queue_free()
-	pass # Replace with function body.
+
+		var w = area.get_meta("weapon", null)
+		if w:
+			weapon = w
+		coins += area.get_meta("coins", 0)
